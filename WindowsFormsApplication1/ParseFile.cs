1c5d46b6f9ca7258fb4dd52372c466b0c8d96ddf
Yuval Feldman <yuval.fe@gigya.net>
2016-12-21 16:09:38 +0200

    Removed spare brakets

--- WindowsFormsApplication1/ParseFile.cs
+++ WindowsFormsApplication1/ParseFile.cs
@@ -179,12 +179,12 @@ namespace Straetusshockomatconverter
                 if (parts[16].Contains('-'))
                 {
                     parts[16] = parts[16].Replace("-", "");
-                    parts[16] = $"\"-{parts[16]}\"";
+                    parts[16] = $"-{parts[16]}";
                 }
                 if (parts[19].Contains('-'))
                 {
                     parts[19] = parts[19].Replace("-", "");
-                    parts[18] = $"\"-{parts[18]}\"";
+                    parts[18] = $"-{parts[18]}";
                 }
 
                 if (parts[6] == null || parts[6] == string.Empty)
